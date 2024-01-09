215866738234dded872c980ce46d3a004c82f6cb
Kousikgvs <kousikgvs@gmail.com>
2024-01-09 21:02:11 +0530

    Fixed The Navbar errors

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import "./NavBar.css"
+import { useData } from '../DataContext'; 
+
 function ResponsiveTopNav() {
+  const { data } = useData();
   const [isResponsive, setIsResponsive] = useState(false);
 
   const handleToggle = () => {
@@ -10,12 +13,15 @@ function ResponsiveTopNav() {
   return (
     <div className={`topnav ${isResponsive ? 'responsive' : ''}`} id="myTopnav">
       <a className="active">Logo Here</a>
-      <div>
-        
+      <div className='user-credentials'>
+        <div className='username'>{data ? data.username : <>Username</>}</div>
+        <button className='logout'>LOGOUT</button>
       </div>
-      <a onClick={handleToggle}>
-        <i className="fa fa-bars"></i>
-      </a>
+      {isResponsive && (
+        <a onClick={handleToggle}>
+          <i className="fa fa-bars"></i>
+        </a>
+      )}
     </div>
   );
 }

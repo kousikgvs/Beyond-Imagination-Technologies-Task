fccb34ec7ebefa3ab52a57335876e1e3aa509549
Kousikgvs <kousikgvs@gmail.com>
2024-01-09 21:14:53 +0530

    Added Sidebar

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -2,11 +2,15 @@ import React from 'react'
 import "./Dashboard.css"
 import { useData } from '../DataContext'; 
 import ResponsiveTopNav from '../NavBar/NavBar';
+import Sidebar from "../SideBar/sidebar"
 const Dashboard = () => {
   const { data } = useData();
   return (
-    <div>
+    <div className='dashbaord'>
     <ResponsiveTopNav />
+    <div className='dashboard-body'>
+      <Sidebar />
+    </div>
     </div>
   )
 }
---
+++ src/SideBar/sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./sidebar.css"
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <h1 className='sidebar-heading'>DASHBOARD</h1>
+    </div>
+  )
+}
+
+export default Sidebar
